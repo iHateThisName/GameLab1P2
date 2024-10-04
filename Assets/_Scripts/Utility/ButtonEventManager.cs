4bbcdd5daa548f290bd18dc2e71fb3f788b46591
Ivar Bredeli <ivabr@stud.ntnu.no>
2024-10-04 17:39:29 +0200

    Level Selector

--- Assets/_Scripts/Utility/ButtonEventManager.cs
+++ Assets/_Scripts/Utility/ButtonEventManager.cs
@@ -18,4 +18,7 @@ public class ButtonEventManager : MonoBehaviour {
     public void OnLevel03() => GameManager.Instance.LoadScene(EnumScene.Level_03);
     public void OnLevel04() => GameManager.Instance.LoadScene(EnumScene.Level_04);
     public void OnLevel05() => GameManager.Instance.LoadScene(EnumScene.Level_05);
+    public void OnLevel06() => GameManager.Instance.LoadScene(EnumScene.Level_01_06);
+    public void OnLevel07() => GameManager.Instance.LoadScene(EnumScene.Level_07);
+    public void OnLevel08() => GameManager.Instance.LoadScene(EnumScene.Level_08);
 }
