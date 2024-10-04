ef31df4fa912a63be0d6ec185f6aa5682a0d797a
Ivar Bredeli <ivabr@stud.ntnu.no>
2024-10-04 11:03:15 +0200

    Feature Added, Player Camera Follow

--- Assets/_Scripts/CheckPointController.cs
+++ Assets/_Scripts/CheckPointController.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckPointController : MonoBehaviour
-{
+public class CheckPointController : MonoBehaviour {
     [SerializeField] private bool _isStartPosition = false;
+    [SerializeField] private bool _useCameraFollow = false;
     void Start() {
         if (_isStartPosition) {
-            GameManager.Instance.SetLevelStartPosition(new Vector2(transform.position.x, transform.position.y));
+            GameManager.Instance.SetLevelStartPosition(new Vector2(transform.position.x, transform.position.y), _useCameraFollow);
         }
     }
 }
--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -9,6 +9,9 @@ public class GameManager : MonoBehaviour {
     public Vector2? LastCheckPointPosition;
     public bool IsPlayerDead { get; private set; } = false;
 
+    private Camera _camera;
+    private bool _cameraFollow = false;
+
     private void Awake() {
         if (Instance == null) {
             Instance = this;
@@ -23,26 +26,39 @@ public class GameManager : MonoBehaviour {
         SceneManager.LoadScene((int)scene);
     }
 
-    public void SetLevelStartPosition(Vector2 position) {
+    public void SetLevelStartPosition(Vector2 position, bool useCameraFollow) {
+        _cameraFollow = useCameraFollow;
         _levelStartPosition = position;
         LastCheckPointPosition = null;
         Respawn();
     }
 
     public void Respawn() {
+        GameObject go;
+
         if (!LastCheckPointPosition.HasValue) {
-            GameObject go = Instantiate(_playerPrefab, _levelStartPosition, Quaternion.identity);
+            go = Instantiate(_playerPrefab, _levelStartPosition, Quaternion.identity);
         } else {
-            GameObject go = Instantiate(_playerPrefab, LastCheckPointPosition.Value, Quaternion.identity);
+            go = Instantiate(_playerPrefab, LastCheckPointPosition.Value, Quaternion.identity);
         }
+        if (_cameraFollow) {
+            if (_camera == null) GetCameraInScene();
+            _camera.transform.SetParent(go.transform);
+            _camera.transform.localPosition = new Vector3(0, 2, -10);  // Adjust this as needed to position the camera correctly
+            _camera.orthographicSize = 5;
+        }
+
         IsPlayerDead = false;
     }
 
     public void Death(GameObject player) {
         if (!IsPlayerDead) {
             IsPlayerDead = true;
+            _camera.gameObject.transform.SetParent(null);
             Destroy(player);
             Respawn();
         }
     }
+
+    private void GetCameraInScene() => _camera = GameObject.FindGameObjectWithTag("MainCamera")?.GetComponent<Camera>();
 }
